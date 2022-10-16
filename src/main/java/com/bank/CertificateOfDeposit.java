da9ed9a8ce43df6ab39ce03e8d6849a454b30471
king2lc <64796478+king2lc@users.noreply.github.com>
2022-10-16 19:32:10 -0400

    Initial Commit
    Pushing completed assignment to Github.

---
+++ src/main/java/com/bank/CertificateOfDeposit.java
@@ -0,0 +1,12 @@
+package com.bank;
+
+public class CertificateOfDeposit extends Account{
+    private int Term;
+    public void setMaturity(int term){
+        this.Term = term;
+    }
+    public int getMaturity(){
+        return Term;
+    }
+}
+
